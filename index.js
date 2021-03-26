2014b3081ce799bfea38d1e6a052f75438d91e95
anaclara-gf <anaclaragf@outlook.com>
2021-03-26 11:47:13 -0300

    bug PORT

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const userRoutes = require('./server/routes/UserRouter');
 const productsRoutes = require('./server/routes/ProductRouter');
 const ordersRoutes = require('./server/routes/OrderRouter');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true}))
